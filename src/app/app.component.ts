2c4a608d6697b4a767770180e3216b5677814ff7
Hugo <albrechthugo4@gmail.com>
2023-08-02 07:56:19 -0300

    chore: html2canvas first approach (blob -> new File)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'png-share';
+  @ViewChild('content', { static: true })
+  private readonly contentRef!: ElementRef<HTMLElement>;
+
+  share(): void {
+    from(html2canvas(this.contentRef.nativeElement)).subscribe((canvas) => {
+      canvas.toBlob(
+        (blob) => {
+          if (!blob) {
+            console.log('Error during blob generating');
+            return;
+          }
+
+          const file = new File([blob], 'users-test', { type: 'image/png' });
+
+          const data = { files: [file] };
+
+          if (!window.navigator.canShare(data)) {
+            console.error('share api not supported here!');
+            return;
+          }
+
+          window.navigator.share(data);
+        },
+        'image/png',
+        1.0
+      );
+    });
+  }
 }
